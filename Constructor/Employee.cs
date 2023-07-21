f1f437bd20a6212ea32fd3190154a1cfc050858c
eswardatt <eswardatta277@gmail.com>
2023-07-21 21:32:47 +0530

    day_2 constructor

---
+++ Constructor/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    class Employee
+    {
+        int a;
+
+        public int b;
+        public Employee(int a)
+        {
+           this.a = a;
+        }
+
+        public void Test()
+        {
+            Console.WriteLine(a);
+        }
+
+
+    }
+}
---
+++ Constructor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Constructor
+{
+    class Program
+    {
+        Program()
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee(10);
+            // employee.b = 10;
+            employee.Test();
+            //Console.WriteLine(x);
+
+        }
+    }
+}
