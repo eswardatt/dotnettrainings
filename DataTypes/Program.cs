010d81b1203a9a4fc7ab13a7ae9f5e485becaf7d
eswardatt <eswardatta277@gmail.com>
2023-07-26 23:19:20 +0530

    datatypes

---
+++ DataTypes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace DataTypes
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            float money = 2000.90f;
+            double salary = 150000.20;
+            int age=20;
+            bool isActive=true;
+            string name="Krishna";
+            //Program program = new Program();
+            //Console.WriteLine("Age : {0}", program.age);
+            //Console.WriteLine("money : {0}", money);
+            //Console.WriteLine("salary : {0}", salary);
+            //Console.WriteLine("name : {0}", program.name);
+            //Console.WriteLine("isactive : {0}", program.isActive);
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append(" Age : ");
+            sb.Append(age);
+            sb.Append("\n Salary : ");
+            sb.Append(salary);
+            sb.Append("\n Name : ");
+            sb.Append(name);
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
