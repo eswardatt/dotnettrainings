766bad790f9230961ef3a55714dc36f1f1f70876
eswardatt <eswardatta277@gmail.com>
2023-08-07 22:59:51 +0530

    delegates

---
+++ delegates/DefaultDelegates.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegates
+{
+    class DefaultDelegates
+    {
+        public string GetFullName(string fname,string lname)
+        {
+            return fname + " " + lname;
+        }
+        public void Division(int a, int b)
+        {
+            Console.WriteLine("Division of {0} & {1} : {2}", a, b, a / b);
+        }
+
+        public bool CheckGender(string gender)
+        {
+            bool genderType = false;
+            if (gender=="male")
+            {
+                genderType = true;
+            }
+            return genderType;
+
+        }
+
+        static void Main()
+        {
+            DefaultDelegates defaultDelegates = new DefaultDelegates();
+
+            Func<string, string, string> func = defaultDelegates.GetFullName;
+            string name =  func.Invoke("Kumar", "Krishna");
+            Console.WriteLine("name : {0}", name);
+
+            Action<int, int> action = defaultDelegates.Division;
+            action.Invoke(20, 10);
+
+            Predicate<string> predicate = defaultDelegates.CheckGender;
+            bool gender = predicate.Invoke("male");
+            Console.WriteLine(gender);
+
+        }
+    }
+}
---
+++ delegates/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace delegates
+{
+    public delegate void DelegateName(string name);
+    public delegate void ArthimaticDelegate(int x, int y);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            DelegateName delegateName = new DelegateName(program.GetName);
+            delegateName.Invoke("Eswar");
+
+            ArthimaticDelegate arthimaticDelegate = new ArthimaticDelegate(program.Add);
+            arthimaticDelegate += program.Mul;
+            arthimaticDelegate.Invoke(10, 10);
+
+            //program.Add(10, 10);
+            //program.Mul(10, 10);
+
+            DelegateName print = delegate (string name)
+              {
+                  Console.WriteLine("Anonymous {0}", name);
+              };
+            print("Datta");
+            //Lamda expressions invented
+            //Lamba expressions are short-hand techique to create anonymous methods
+            DelegateName print_1 = (x) => { Console.WriteLine("Lamba expressions : {0}", x); };
+            print_1("is short-hand technique of Anonumous methods");
+        }
+
+        public void GetName(string name)
+        {
+            Console.WriteLine("Name is : {0}",name);
+        }
+
+        public void Add(int a, int b)
+        {
+            Console.WriteLine("sum is : {0}", a+b);
+        }
+
+        public void Mul(int a, int b)
+        {
+            Console.WriteLine("multiply is : {0}", a * b);
+        }
+
+        //public void GetName(string name)
+        //{
+        //    Console.WriteLine(name);
+        //}
+    }
+}
