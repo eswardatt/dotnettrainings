f178eccc8f07160fad96df42a88117d2989eec56
eswardatt <eswardatta277@gmail.com>
2023-08-22 00:11:12 +0530

    singleton

---
+++ designpattrens/singletonPattern/Normal.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singletonPattern
+{
+   
+   sealed class Normal
+    {
+        public static int id = 0;
+        private static readonly object obj = new object();
+
+        private Normal()
+        {
+            id++;
+            Console.WriteLine("Normal class {0}", id);
+        }
+
+        private static Normal instance = null;
+
+        public static Normal GetInstance()
+        {
+            // double checked lock 
+            if (instance == null)
+            {
+                lock (obj)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Normal();
+                    }
+                }
+            }
+           
+          
+            return instance;
+        }
+
+        public void GetName(string name)
+        {
+            Console.WriteLine($"Name : {name}");
+        }
+      
+      
+        
+    }
+}
---
+++ designpattrens/singletonPattern/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using static singletonPattern.Normal;
+
+namespace singletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Parallel.Invoke(
+                ()=>GetTeacher(),
+                ()=>GetStudent()
+                );
+        }
+
+        public static void GetTeacher()
+        {
+            Normal normal_1 = Normal.GetInstance();
+            normal_1.GetName("Teacher");
+        }
+
+        public static void GetStudent()
+        {
+            Normal normal = Normal.GetInstance();
+            normal.GetName("Student");
+        }
+    }
+}
